4a5ccd14883db68f73c7c6e046d5f8264016029f
Jitendra <jitendrasingh14355@gmail.com>
2020-08-30 23:43:35 +0530

    create fields and required constants in FrontDesk.java class

--- src/FrontDesk.java
+++ src/FrontDesk.java
@@ -7,4 +7,18 @@
 
 public class FrontDesk {
 
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final int ISSUE_NEW_BOOK = 1;
+    public static final int RETURN_ISSUED_BOOK = 2;
+    public static final int SHOW_ISSUED_BOOKS = 3;
+    public static final int EXIT = 4;
+    Student studentDetails;
 }
